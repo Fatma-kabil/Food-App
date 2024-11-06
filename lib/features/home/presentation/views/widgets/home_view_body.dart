584f31efbc5c674e6453c6d3dbbaa5423fbead9b
Fatma_kabil <fatmakhaled112@gmail.com>
2024-11-06 16:02:10 +0200

    creare home view

---
+++ lib/features/home/presentation/views/widgets/home_view_body.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:food_app/constants.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(width: width(context),height: height(context),child: Column(),);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:food_app/features/home/presentation/views/home_view.dart';
 
 void main() {
   runApp(const
    FoodApp());
 }
 
-class FoodApp extends StatelessWidget {
+class FoodApp extends StatefulWidget {
   const FoodApp({super.key});
 
+  @override
+  State<FoodApp> createState() => _FoodAppState();
+}
+
+class _FoodAppState extends State<FoodApp> {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
+      title: 'Flutter Ui',
     debugShowCheckedModeBanner: false,
+    home: HomeView(),
     );
   }
 }
